f8fa91f4aeda71a31a233445e1d146cce25aa329
psrpasap <psrpasap@gmail.com>
2018-05-07 17:03:39 -0400

    Changes for checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * This is greet method
+   * @param String
+  **/
+  public final String greet(final String someone) {
     return String.format("Hello Prasad, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/package-info.java
@@ -0,0 +1,6 @@
+/**
+*
+*package information
+**/
+
+package com.example;
